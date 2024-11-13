ccc11d3cbb85c9f6cf5409a6d139a1912e61aedc
fahrifuh <muhammadfahri571@gmail.com>
2024-11-13 22:08:44 +0700

    menambah middleware untuk dashboard dan login

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Http\Controllers\AnggotaKoperasiController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
+
+
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [LoginController::class, 'showForm'])->name('login');
 });
 
-Route::resource('data-anggota', AnggotaKoperasiController::class);
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [DashboardController::class])->name('dashboard');
+    Route::resource('data-anggota', AnggotaKoperasiController::class);
+});
