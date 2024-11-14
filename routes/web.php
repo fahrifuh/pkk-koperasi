51d8828eedd4d455f26c56e82451f00f749d13dd
fahrifuh <muhammadfahri571@gmail.com>
2024-11-14 19:32:58 +0700

    fixed some errors while redirect after login

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest'])->group(function () {
     Route::get('/login', [LoginController::class, 'showForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/', [DashboardController::class])->name('dashboard');
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     Route::resource('data-anggota', AnggotaKoperasiController::class);
 });
