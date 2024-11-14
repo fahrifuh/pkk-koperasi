7b3ae699a54a5b06121f69b16b0135d1a09bf01f
fahrifuh <muhammadfahri571@gmail.com>
2024-11-14 20:17:16 +0700

    added role check

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index(){
-        
+    public function index()
+    {
+        if (Auth::check()) {
+            if (Auth::user()->role == 'admin') {
+                return view('dashboard');
+            } else {
+                return view('kelola-koperasi.index');
+            }
+        }
     }
 }
