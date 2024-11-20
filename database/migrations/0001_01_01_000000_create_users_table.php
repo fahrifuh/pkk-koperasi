f9de7d555ef566a2f26690ea32e716658d2341b1
fahrifuh <muhammadfahri571@gmail.com>
2024-11-20 19:46:15 +0700

    fix some migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,11 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('username');
             $table->string('password');
+            $table->string('ori_password');
+            $table->string('role')->default('warga');
+            $table->unsignedBigInteger('id_anggota')->default(0);
             $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('id_anggota')->constrained('anggota_koperasi')->onDelete('cascade');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
