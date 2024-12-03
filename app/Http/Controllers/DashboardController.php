1339aa2a441bb5576af8a9f0edc0a16c5760ff60
fahrifuh <muhammadfahri571@gmail.com>
2024-12-03 18:41:07 +0700

    make adjustment for some features

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\AnggotaKoperasi;
+use App\Models\Transaksi;
 use App\Models\Warga;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -11,13 +12,16 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        $anggotaID = Auth::user()->id_anggota;
         $countAnggota = AnggotaKoperasi::count();
         $countWarga = Warga::count();
+        $countTransaksi = Transaksi::where('id_anggota', $anggotaID)->count();
+        $sumTransaksi = Transaksi::where('id_anggota', $anggotaID)->sum('jumlah');
         if (Auth::check()) {
             if (Auth::user()->role == 'admin') {
                 return view('dashboard', compact('countAnggota', 'countWarga'));
             } else {
-                return redirect()->route('data-anggota.index');
+                return view('wargaDashboard', compact('countTransaksi', 'sumTransaksi'));
             }
         }
     }
