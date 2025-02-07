eba590d527a6d9a29bcdab36952facf714805136
fahrifuh <muhammadfahri571@gmail.com>
2025-02-07 14:04:05 +0700

    setup pwa

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,32 +1,49 @@
 /*!
-    * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
-    * Copyright 2013-2023 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    // 
+ * Start Bootstrap - SB Admin v7.0.7 (https://startbootstrap.com/template/sb-admin)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
+ */
+//
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
-
+window.addEventListener("DOMContentLoaded", (event) => {
     // Toggle the side navigation
-    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    const sidebarToggle = document.body.querySelector("#sidebarToggle");
     if (sidebarToggle) {
         // Uncomment Below to persist sidebar toggle between refreshes
         // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
         //     document.body.classList.toggle('sb-sidenav-toggled');
         // }
-        sidebarToggle.addEventListener('click', event => {
+        sidebarToggle.addEventListener("click", (event) => {
             event.preventDefault();
-            document.body.classList.toggle('sb-sidenav-toggled');
-            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+            document.body.classList.toggle("sb-sidenav-toggled");
+            localStorage.setItem(
+                "sb|sidebar-toggle",
+                document.body.classList.contains("sb-sidenav-toggled")
+            );
         });
     }
-
 });
 
-function handleArrowKey(event){
-    if(event.key == "ArrowUp" || event.key == "ArrowDown"){
+function handleArrowKey(event) {
+    if (event.key == "ArrowUp" || event.key == "ArrowDown") {
         event.preventDefault();
     }
+}
+
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/service-worker.js").then(
+            function (registration) {
+                console.log(
+                    "Service Worker registered with scope:",
+                    registration.scope
+                );
+            },
+            function (err) {
+                console.log("Service Worker registration failed:", err);
+            }
+        );
+    });
 }
